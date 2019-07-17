6f714db54b4cb73cbdda0f4d4eddbdc4707b5d65
Brandon Trost <trostbd@gmail.com>
2019-07-17 16:16:45 -0400

    RxJava

--- app/src/main/java/com/example/moviedbassignment/helpers/RetrofitHelper.kt
+++ app/src/main/java/com/example/moviedbassignment/helpers/RetrofitHelper.kt
@@ -1,13 +1,21 @@
 package com.example.moviedbassignment.helpers
 
 import android.arch.lifecycle.MutableLiveData
+import android.util.Log
 import com.example.moviedbassignment.model.Movie
 import com.example.moviedbassignment.model.MovieList
+import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.SingleObserver
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import okhttp3.OkHttpClient
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -17,6 +25,7 @@ class RetrofitHelper(url: String, client: OkHttpClient) {
     val retrofit = Retrofit.Builder()
         .baseUrl(url)
         .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .client(client)
         .build()
     val service = retrofit.create(RemoteService::class.java)
@@ -24,7 +33,7 @@ class RetrofitHelper(url: String, client: OkHttpClient) {
     fun retrieveMovieList(): MutableLiveData<MovieList> {
         val call =service.getMovies(API_KEY, LANGUAGE_ENGLISH, SORT_BY_POP_DESC, INCLUDE_ADULT_FALSE, INCLUDE_VIDEO_FALSE, PAGE_COUNT)
         val movieListData =MutableLiveData<MovieList>()
-        call.enqueue(object: Callback<MovieList> {
+        /*call.enqueue(object: Callback<MovieList> {
             override fun onFailure(call: Call<MovieList>, t: Throwable) {
                 t.printStackTrace()
                 movieListData.value = MovieList(ArrayList<Movie>())
@@ -36,6 +45,20 @@ class RetrofitHelper(url: String, client: OkHttpClient) {
                 }
             }
 
+        })*/
+        call.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(object :
+            SingleObserver<MovieList> {
+            override fun onSuccess(t: MovieList) {
+                movieListData.value = t
+            }
+
+            override fun onSubscribe(d: Disposable) {
+            }
+
+            override fun onError(e: Throwable) {
+                Log.e("ERROR",e.message)
+            }
+
         })
         return movieListData
     }
@@ -44,6 +67,6 @@ class RetrofitHelper(url: String, client: OkHttpClient) {
         @GET("discover/movie")
         fun getMovies(@Query("api_key")api_key: String, @Query("language")language: String,
                       @Query("sort_by")sort_by: String, @Query("include_adult")include_adult: Boolean,
-                      @Query("include_video")include_video: Boolean, @Query("page")page: Int): Call<MovieList>
+                      @Query("include_video")include_video: Boolean, @Query("page")page: Int): Single<MovieList>
     }
 }
